ae05659b48d1fed9423e8205b298fc2f3c6f70a0
Patricia Garcia <pat@patriciagarcia.me>
2016-07-31 11:15:01 +0200

    feat: add stock-status-trend.component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import angular from 'angular'
 
 import stockStatusRibbon from './stock-status-ribbon.component'
+import stockStatusTrend from './stock-status-trend.component'
 
 angular
   .module('angularNavStockStatusRibbon', [])
   .component('stockStatusRibbon', stockStatusRibbon)
+  .component('stockStatusTrend', stockStatusTrend)
---
+++ src/stock-status-trend.component.js
@@ -0,0 +1,10 @@
+import stockStatusTrendTemplate from './stock-status-trend.html'
+
+const stockStatusTrend = {
+  template: stockStatusTrendTemplate,
+  bindings: {
+    trend: '<'
+  }
+}
+
+export default stockStatusTrend
